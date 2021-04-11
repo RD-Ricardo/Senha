c2643ccc1d120a2179cbaaa44b5e49911cdd56d5
RD <ricardodias169@gmail.com>
2021-04-10 22:17:06 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "1234abcd";
+            string entraSenha;
+            Console.WriteLine("---------");
+            Console.WriteLine("--SENHA--");
+            Console.WriteLine("---------\n");
+            Console.Write("Olá, usuário. Por favor, digite sua senha:");
+            entraSenha = Convert.ToString(Console.ReadLine());
+            if(entraSenha == senha){
+                Console.WriteLine("Acesso permitido");
+            }
+            else{
+                Console.WriteLine("\nAcesso negado");
+            }
+        }
+
+    }
+}
